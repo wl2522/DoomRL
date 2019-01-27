d3ebbb46cec8efd7245864f6cde512f337477a2b
Wilson Lui <wl2522@columbia.edu>
2019-01-27 01:46:29 -0500

    committing a working example of the frame-skipping algorithm

--- frame_skip.py
+++ frame_skip.py
@@ -1,25 +1,29 @@
 from collections import deque
 
 buffer = list()
-stack = deque()
+queue = deque()
+
+# Push four empty lists into the stack to start the frame-skipping algorithm
+for i in range(4):
+    queue.append(list())
+
+# Use a counter to keep track of how many frames have been proccessed
 counter = 0
 
-for i in range(4):
-    stack.append(list())
-
 for frame in range(64):
-    if (counter + 1) % 4 == 0:
-        for i in range(counter//4, (counter + 4)//4 + 3):
-            stack[i].append(frame)
-#        if counter > 6 and counter <= 15:
-#            stack.popleft()
-
-        if len(stack[3]) == 4:
-            phi = stack.pop()
-            buffer.append(phi)
-            stack.append(list())
-        stack.append(list())
-
+    # Increment the counter first so that we can check for divisibility by 4
     counter += 1
-    print(counter, stack)
-print(buffer, stack, counter)
+    # Process only every 4th frame
+    if counter % 4 == 0:
+        for i in range(4):
+            queue[i].append(frame//4)
+
+
+        phi = queue.popleft()
+        # Begin adding stacks to the buffer once there are 4 in the oldest one
+        if len(phi) == 4:
+            buffer.append(phi)
+        # Replace the stack we just popped with a new one
+        queue.append(list())
+
+print(buffer)
