2f0b8e1a4f7b36aefbd6f7e067148f0b9eeb97df
Wilson Lui <wl2522@columbia.edu>
2019-02-17 13:53:54 -0500

    refactored the script to use the start_game() and get_game_params() functions from helper.py

--- take_cover/take_cover.py
+++ take_cover/take_cover.py
@@ -1,11 +1,11 @@
-"""For each time step, collect the following data:
+"""
+For each time step, collect the following data:
     1. The current game state
     2. The action that was taken taken
     3. The reward obtained from the chosen action
     4. The next game state
         (store the first game state if the previous action ends the episode)
     5. A variable indicating whether the episode is over yet
-
 """
 
 import yaml
@@ -13,7 +13,7 @@ import tensorflow as tf
 import numpy as np
 import vizdoom as vd
 from tqdm import trange
-from helper import preprocess, test_agent
+from helper import start_game, get_game_params, preprocess, test_agent
 from q_network import QNetwork, update_graph, update_target
 from buffer import Buffer
 
@@ -25,24 +25,14 @@ with open('take_cover.yml') as config_file:
     config = yaml.load(config_file)
 
 # Specify the game scenario and the screen format/resolution
-game = vd.DoomGame()
-game.set_screen_format(vd.ScreenFormat.BGR24)
-game.set_screen_resolution(vd.ScreenResolution.RES_640X480)
-game.set_depth_buffer_enabled(config['enable_depth_buffer'])
-game.load_config('take_cover.cfg')
+game = start_game(screen_format=vd.ScreenFormat.BGR24,
+                  screen_res=vd.ScreenResolution.RES_640X480,
+                  config='take_cover.cfg',
+                  depth=config['enable_depth_buffer'],
+                  sound=config['enable_sound'])
 
-down_sample_ratio = config['down_sample_ratio']
-width = int(game.get_screen_width()*down_sample_ratio)
-height = int(game.get_screen_height()*down_sample_ratio)
-
-# Add an extra channel to accomodate the depth buffer if it's enabled
-channels = game.get_screen_channels() + int(game.is_depth_buffer_enabled())
-
-# Specify the available actions in the scenario
-actions = game.get_available_buttons()
-
-# Create a list of one hot encoded lists to represent each possible action
-actions = [list(ohe) for ohe in list(np.identity(len(actions)))]
+width, height, channels, actions = get_game_params(game,
+                                                   config['down_sample_ratio'])
 
 # Define the Q-network learning parameters
 frame_delay = config['frame_delay']
@@ -53,7 +43,7 @@ phase1_len = config['phase1_ratio']*epochs
 phase2_len = config['phase2_ratio']*epochs
 
 steps_per_epoch = config['steps_per_epoch']
-learning_rate =  config['learning_rate']
+learning_rate = config['learning_rate']
 gamma = config['gamma']
 start_epsilon = config['start_epsilon']
 end_epsilon = config['end_epsilon']
